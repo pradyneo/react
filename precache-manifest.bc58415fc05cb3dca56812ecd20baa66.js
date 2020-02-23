9d3b3deb1c1043ba1e8e6a59acce61fa8288154b
Prady <pradyneo@users.noreply.github.com>
2020-02-22 22:03:34 -0500

    Updates

---
+++ precache-manifest.bc58415fc05cb3dca56812ecd20baa66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c87818a5012bdaf5acc206311ec183",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "b0a77bfdcd6fe6940e3e",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "59bbb4eeff314a7e8b04",
+    "url": "/react/static/js/2.fa81cbda.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react/static/js/2.fa81cbda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a77bfdcd6fe6940e3e",
+    "url": "/react/static/js/main.1ddff40e.chunk.js"
+  },
+  {
+    "revision": "7f1756c7e3e788db3ccb",
+    "url": "/react/static/js/runtime-main.cfe49737.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
